ce807ce78a450d9175340f742277b88b394794aa
Austin Seipp <aseipp@pobox.com>
2014-08-14 13:10:01 -0500

    bot: Fix dumbness
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/extensions/bot/handler/PhabricatorBotMiscGhcHandler.php
+++ src/extensions/bot/handler/PhabricatorBotMiscGhcHandler.php
@@ -9,6 +9,7 @@ final class PhabricatorBotMiscGhcHandler extends PhabricatorBotHandler {
     switch ($message->getCommand()) {
       case 'MESSAGE':
         $target_name = $message->getTarget()->getName();
+        $text        = $message->getBody();
         if ($target_name !== '#ghc') {
           // Don't do this in non-GHC channels, as it's probably annoying.
           break;
@@ -16,7 +17,7 @@ final class PhabricatorBotMiscGhcHandler extends PhabricatorBotHandler {
 
         /* -- Case #1: Check for 'reportabug' and link to  Trac -- */
         $matches = null;
-        if (preg_match('/reportabug/i')) {
+        if (preg_match('/reportabug/i', $text, $matches)) {
           $this->replyTo($message,
             pht('https://ghc.haskell.org/trac/ghc/newticket?type=bug'));
           break;
