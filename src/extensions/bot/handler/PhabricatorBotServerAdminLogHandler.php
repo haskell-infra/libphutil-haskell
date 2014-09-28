edc5c0273b32a0576e3d71247a07fd11c9881c16
Ricky Elrod <ricky@elrod.me>
2014-09-27 22:10:28 -0400

    First go at logging logs for the log log log
    Summary:
    Implement the TODOs that @austin left.
    Some of this could probably be cleaned up, but this is a first go.
    
    Test Plan: Tested a bunch locally in #relrod on freenode.
    
    Reviewers: austin, #haskell.org_infrastructure
    
    Reviewed By: austin, #haskell.org_infrastructure
    
    Subscribers: austin
    
    Differential Revision: https://phabricator.haskell.org/D257

--- src/extensions/bot/handler/PhabricatorBotServerAdminLogHandler.php
+++ src/extensions/bot/handler/PhabricatorBotServerAdminLogHandler.php
@@ -36,6 +36,35 @@ final class PhabricatorBotServerAdminLogHandler extends PhabricatorBotHandler {
       ));
   }
 
+  private function affirmative() {
+    $affirmatives = array(
+      pht('Indubitably.'),
+      pht('You betcha!'),
+      pht('Indeed.'),
+      pht('You got it!'),
+      pht('Aye-aye!'),
+      pht('beep-boop beep-boop. Affirmative.'),
+      pht('Uh huh!'),
+      pht('Consider it done.'),
+      pht('Anything for you!'),
+      pht('Look at you, being all productive!'),
+      pht('You silly sysadmin!'),
+    );
+    return $affirmatives[array_rand($affirmatives)];
+  }
+
+  private function linkIrcUser($nickname) {
+    $nick_map = array(
+      'thoughtpolice' => 'austin',
+    );
+
+    if (array_key_exists($nickname, $nick_map)) {
+      return '@'.$nick_map[$nickname];
+    } else {
+      return '@'.$nickname;
+    }
+  }
+
   public function receiveMessage(PhabricatorBotMessage $message) {
     switch ($message->getCommand()) {
       case 'MESSAGE':
@@ -50,14 +79,82 @@ final class PhabricatorBotServerAdminLogHandler extends PhabricatorBotHandler {
         /* -- Case #1: Check for 'log "foobar"' and add it. -- */
         $matches = null;
         if (preg_match("/$this->botName: log \"(.+)\"/i", $text, $matches)) {
-          $this->replyTo($message, pht('TODO FIXME'));
+          $today = date('Y-m-d');
+          $date = null;
+          $raw_lines = explode("\n", $this->getWikiPageContent());
+
+          // $linenum starts at 1. This because if we don't find today's date
+          // below, then we insert it (at line 0) and log immediately below it.
+          $linenum = 1;
+          $found_date = false;
+          $i = 0;
+          foreach ($raw_lines as $line) {
+            if (preg_match('/=== Date: (.+) ===/i', $line, $date) &&
+                $date[1] == $today) {
+              $linenum = $i + 1;
+              $found_date = true;
+              break;
+            }
+            $i++;
+          }
+
+          if (!$found_date) {
+            $date_fmt = '=== Date: '.$today.' ===';
+            array_splice($raw_lines, 0, 0, $date_fmt);
+          }
+
+          $sender_nick = $this->linkIrcUser($message->getSender()->getName());
+          $msg = ' - '.$sender_nick.': '.$matches[1];
+          array_splice($raw_lines, $linenum, 0, $msg);
+
+          // Pedantic formatting silliness - add a newline after the new
+          // section. Get the line number of the date line, skip over the log
+          // line, then insert a newline.
+          if (!$found_date) {
+            array_splice($raw_lines, $linenum + 1, 0, "\n");
+          }
+
+          $this->editWikiPage(implode("\n", $raw_lines));
+          $this->replyTo($message, $this->affirmative());
           break;
         }
 
         /* -- Case #2: Check for 'get log' and get the lastest entries. --  */
         $matches = null;
         if (preg_match("/$this->botName: get log(s?)/i", $text, $matches)) {
-          $this->replyTo($message, pht('TODO FIXME'));
+          $events = array();
+          $raw_lines = explode("\n", $this->getWikiPageContent());
+          $date = null;
+          $last_date = null;
+          $logline = null;
+          foreach ($raw_lines as $line) {
+            if (preg_match('/=== Date: (.+) ===/i', $line, $date)) {
+              $last_date = $date[1];
+              $events[$last_date] = array();
+            }
+            if (preg_match('/ - (.+)/i', $line, $logline)) {
+              $events[$last_date][] = $logline[1];
+            }
+          }
+
+          $formatted_events = array();
+          foreach ($events as $date => $actions) {
+            $date = '['.chr(2).$date.chr(2).'] ';
+            foreach ($actions as $action) {
+              $formatted_events[] = $date.$action;
+            }
+          }
+
+          $i = 0;
+          foreach ($formatted_events as $e) {
+            if ($i == $this->numLogs) {
+              break;
+            }
+
+            $this->replyTo($message, $e);
+            $i++;
+          }
+
           break;
         }
 
